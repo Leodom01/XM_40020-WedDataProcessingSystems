393ae509846b78cd9d40eb6029f578fa88ea8cd4
Quartinocci <96389802+Quartinocci@users.noreply.github.com>
2023-11-27 19:14:42 +0100

    Delete buildAndStartContainer.sh

--- buildAndStartContainer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build Docker image
-docker build -t llama_fact_checker:latest .
-
-echo "You will now be redirected to the container shell, the shell location is already inside the shared folder of the repo."
-
-# Run the Docker image with shared folder
-docker run -v "$(pwd)":/sharedFolder -it llama_fact_checker:latest /bin/bash
