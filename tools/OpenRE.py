d728fdedfb349770d44649c5b27cbc481d11de6e
Omid Afroozeh <afroozeh.omid@gmail.com>
2023-12-05 22:53:37 +0100

    removed opnenre/fixed stanfordIE

--- tools/OpenRE.py
+++ tools/OpenRE.py
@@ -1,50 +1,34 @@
-from openie import StanfordOpenIE
-import itertools
-import sys
-sys.path.append("..")
-import opennre
+import stanza
+from stanza.server import CoreNLPClient
 '''
 This module is for open relation extraction on raw text using the Stanford Open IE
 https://nlp.stanford.edu/software/openie.html
 Python wrapper https://github.com/philipperemy/stanford-openie-python
 '''
 class OpenRE:
-    def __init__(self, text):
-        self.text = text
+    def __init__(self):
+        stanza.install_corenlp()
 
-    def extract_relations_stanford(self):
-        # https://stanfordnlp.github.io/CoreNLP/openie.html#api
-        # Default value of openie.affinity_probability_cap was 1/3.
-        properties = {
-            'openie.affinity_probability_cap': 1 / 3,
-        }
-        with StanfordOpenIE(properties=properties) as client:
-            annotated_text = client.annotate(self.text)
-            for triple in annotated_text:
-                print('|-', triple)
-            # to generate an image of the relations extracted, must install graphviz,
-            # graph_image = 'graph.png'
-            # client.generate_graphviz_graph(self.text, graph_image)
-            # print('Graph generated: %s.' % graph_image)
+    def extract_relations_stanford(self, text):
+        client = CoreNLPClient(timeout=150000000, be_quiet=True, annotators=['openie'],
+                               endpoint='http://localhost:9001')
+        client.start()
 
-    def extract_relations_openNRE(self, annotated_doc, threshhold):
+        document = client.annotate(text, output_format='json')
         triples = []
-        model = opennre.get_model('wiki80_cnn_softmax')
-        for sent in annotated_doc.sents:
-            for permutation in itertools.permutations(set(sent.ents), 2):
-                source = permutation[0]
-                target = permutation[1]
-                input = {
-                    'text': sent.text,
-                    'h': {'pos': (source.start_char - sent.start_char, source.end_char - sent.start_char)},
-                    't': {'pos': (target.start_char - sent.start_char, target.end_char - sent.start_char)}}
-                res = model.infer(input)
-                if res[1] >= threshhold:
-                    triples.append((source.text, target.text, res[0]))
+        for sentence in document['sentences']:
+            for triple in sentence['openie']:
+                triples.append({
+                    'subject': triple['subject'],
+                    'relation': triple['relation'],
+                    'object': triple['object']
+                })
         return triples
 
 if __name__ == "__main__":
-    text = 'Obama was born in Hawaii. Elon, The American investor, founded tesla'
-    OIE = OpenRE(text)
-    OIE.extract_relations_stanford()
+    text = 'Obama was born in Hawaii. Elon founded tesla. He also founded SpaceX'
+    OIE = OpenRE()
+    triples = OIE.extract_relations_stanford(text)
+    for triple in triples:
+        print(triple)
 
